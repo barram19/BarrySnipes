c66e03833040fb8a3f1fbf9e37d640f7c1e188f7
barram19 <131893869+barram19@users.noreply.github.com>
2024-09-20 08:26:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def add_to_history(user_input, ai_response):
 # Function to display the chat history
 def display_chat_history():
     for chat in st.session_state['history']:
-        message(chat["user"], is_user=True, avatar_style=" ", seed="user_message")  # Display user message with user=True
-        message(chat["bot"], avatar_style=" ", seed="bot_message")  # Display bot response
+        message(chat["user"], is_user=True, avatar=" ", seed="user_message")  # Display user message with user=True
+        message(chat["bot"], avatar=" ", seed="bot_message")  # Display bot response
 
 # Create a placeholder for the chat history (to ensure it's above the input box)
 chat_placeholder = st.container()
